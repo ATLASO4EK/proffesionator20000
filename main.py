c300bfbe094442153af4ee423ad9a4452801524e
Graff1029 <zagajnovsasa6@gmail.com>
2025-09-30 21:25:14 +0700

    чтото не рабочее

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from PyQt6.QtCore import Qt
 from PyQt6.QtWidgets import (
     QMainWindow, QApplication,
     QLabel, QCheckBox, QComboBox, QLineEdit,
-    QLineEdit, QSpinBox, QDoubleSpinBox, QSlider
+    QLineEdit, QSpinBox, QDoubleSpinBox, QSlider, QListWidget, QVBoxLayout, QHBoxLayout
 )
 from PyQt6.QtCore import Qt
 
@@ -19,17 +19,60 @@ class MainWindow(QMainWindow):
         widget.setFont(font)
         widget.setAlignment(Qt.AlignmentFlag.AlignHCenter | Qt.AlignmentFlag.AlignTop)
         
+        podzagalovok = QLabel("Маленький текст")
+        podzagalovok_font = podzagalovok.font()
+        podzagalovok_font.setPointSize(23)
+        podzagalovok.setAlignment(Qt.AlignmentFlag.AlignLeft)
 
-        self.setCentralWidget(widget)
+        vopros_1_label = QLabel("1. Тебе нравится бла-бла-бла")
+        vopros_1_font = vopros_1_label.font()
+        vopros_1_font.setPointSize(16)
+        vopros_1_label.setFont(vopros_1_font)
     
+        vopros_1_1 = QCheckBox("10/10")
+        vopros_1_1.stateChanged.connect(self.vopros_1_chaged_1)
+        vopros_1_2 = QCheckBox("9/10")
+        vopros_1_2.stateChanged.connect(self.vopros_1_chaged_2)
+        vopros_1_3 = QCheckBox("8/10")
+        vopros_1_3.stateChanged.connect(self.vopros_1_chaged_3)
 
-    
-        boxis = QCheckBox()# тут 
-        boxis.setCheckState(Qt.CheckState.Checked)
-        boxis.stateChanged.connect(self.show_state)
+        spisok = QComboBox("На сколько вы готовы")
+        spisok.addItems(["1", "2", "3"])
+        spisok.currentIndexChanged.connect(self.index_xhanged)
+        spisok.editTextChanged.connect(self.text_xhanged)
+
+        neskolko = QListWidget("дада выбирай скока хочешь")
+        neskolko.addItems(["One", "Two", "Three"])
+        neskolko.currentItemChanged.connect(self.index_changed)
+        neskolko.currentTextChanged.connect(self.text_changed)
+
+        texito = QLineEdit("Введите текс")
+        texito.setMaxLength(10)
+        texito.setPlaceholderText("Enter your text")
+        texito.returnPressed.connect(self.return_pressed)
+        texito.selectionChanged.connect(self.selection_changed)
+        texito.textChanged.connect(self.text_changed)
+        texito.textEdited.connect(self.text_edited)
+    def return_pressed(self):
+        print("Return pressed!")
+        self.centralWidget().setText("BOOM!")
+
+    def selection_changed(self):
+        print("Selection changed")
+        print(self.centralWidget().selectedText())
+
+    def text_changed(self, s):
+        print("Text changed...")
+        print(s)
+
+    def text_edited(self, s):
+        print("Text edited...")
+        print(s) 
+
+        layout.addWidget(widget)
+
     
         
-        self.setCentralWidget(boxis)
         
 
     def show_state(self, s):
